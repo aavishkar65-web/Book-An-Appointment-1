7f0aa9204ebca0856b004f412387b68d3dac48e8
user <user@ASPL59>
2026-04-24 17:38:30 +0530

    Add project files.

---
+++ Book An Appointment/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Book_An_Appointment.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
